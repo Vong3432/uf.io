e240d1318007787acda19612f5ff8d7d6f5fac19
Vong3432 <vongnyuksoon2000@gmail.com>
2021-02-11 03:48:00 +0800

    style(code): Add syntax highlighter

--- components/main/Code.js
+++ components/main/Code.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import prettify from 'html-prettify'
+import { LightAsync as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 const Code = ({ code, css, html, js }) => {
     
@@ -7,22 +9,24 @@ const Code = ({ code, css, html, js }) => {
 
     useEffect(() => {
 
-        console.log(css)
+        console.table(SyntaxHighlighter.supportedLanguages);
         
         if(code)
-            setCurrentTab("HTML");
+            setCurrentTab("html");
 
         return () => {
             console.log('cleanup')
         }
-    }, [code])
+    }, [code])    
+    
 
     return (
         <div className="editor-container">  
             {code && <div className="editor-tabs">
-                {["HTML", "CSS", "JS"].map((tab) => <button onClick={() => setCurrentTab(tab)} className={`${currentTab === tab ? 'selected' : ''}`}>{tab}</button>)}
+                {["html", "css", "javascript"].map((tab) => <button onClick={() => setCurrentTab(tab)} className={`${currentTab === tab ? 'selected' : ''}`}>{tab.toUpperCase()}</button>)}
             </div>}                      
-            {code ? <pre className="prettyprint">{currentTab === "HTML" ? html : currentTab === "CSS" ? css : currentTab === "JS" ? js : ''}</pre> : <h3>Click a component to view source code.</h3> }            
+            {/* {code ? <pre className="prettyprint">{currentTab === "html" ? html : currentTab === "css" ? css : currentTab === "javascript" ? js : ''}</pre> : <h3>Click a component to view source code.</h3> }             */}
+            {code ? <SyntaxHighlighter style={atomOneDarkReasonable} language={currentTab === "html" ? "htmlbars" : currentTab}>{currentTab === "html" ? html : currentTab === "css" ? css : currentTab === "javascript" ? js : ''}</SyntaxHighlighter> : <h3>Click a component to view source code.</h3> }            
         </div>
     )
 }
--- pages/components/[pid].js
+++ pages/components/[pid].js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import WidgetPage from '../../components/widget/Widget'
+import Head from 'next/head'
 
 // https://stackoverflow.com/a/56394170/10868150
 
@@ -17,8 +18,23 @@ const ComponentDetail = () => {
     }, [pid])
 
     return (
-        <div className="">
+        <div className="">            
             {isLoading ? 'Loading' : <>
+                <Head>
+                    <title>UF.io - {pid.toUpperCase()}</title>
+                    <meta name="description" content="Made for developers to explore ux-first components." />
+                    {/* <link rel="icon" href="/assets/svg/logo.svg" /> */}
+
+                    <meta property="og:title" content="UF.io - Open-source UX first components for developers" />
+                    <meta property="og:description" content="Made for developers to explore ux-first components." />
+                    {/* <meta property="og:image" content="https://pixelify.vercel.app/assets/img/meta.png"/> */}
+                    {/* <meta property="og:url" content="https://pixelify.vercel.app/"></meta> */}
+
+                    <meta name="twitter:title" content="UF.io - Open-source UX first components for developers" />
+                    <meta name="twitter:description" content="Made for developers to explore ux-first components." />
+                    {/* <meta name="twitter:image" content="https://pixelify.vercel.app/assets/img/meta.png" /> */}
+                    {/* <meta name="twitter:card" content="https://pixelify.vercel.app/assets/img/meta.png"></meta> */}
+                </Head>
                 <WidgetPage source={pid} />
             </>}            
         </div>
